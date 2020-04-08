ee9bd8b950a356d341d4e7c55f7493b3faee9565
PeterMPhillips <github@pmphillips.ca>
2020-04-08 12:30:01 -0700

    fix: updated repository with hexid and changed id used in issue data

--- src/projects.ts
+++ src/projects.ts
@@ -17,16 +17,16 @@ import {
   ScriptResult,
   RecoverToVault,
 } from "../generated/templates/Projects/Projects"
-import { 
-  Repo, 
-  RepoData, 
-  Issue, 
-  IssueData, 
-  Labels, 
-  Author, 
-  ProxyAddress, 
-  FundingHistory, 
-  User, 
+import {
+  Repo,
+  RepoData,
+  Issue,
+  IssueData,
+  Labels,
+  Author,
+  ProxyAddress,
+  FundingHistory,
+  User,
   BountyIssue,
   Application,
   Review,
@@ -71,6 +71,7 @@ export function handleRepoAdded(event: RepoAdded): void {
   let data = RepoData.load(id)
   if (data == null) {
     data = new RepoData(id)
+    data.hexId = id
   }
   if (!event.params.decoupled) {
     data._repo = event.params.repoId.toString()
@@ -81,13 +82,13 @@ export function handleRepoAdded(event: RepoAdded): void {
   data.decoupled = event.params.decoupled
   data.repoData = repoContractData.value3
   //data.repo = event.params.repoId.toHex()
-  //if (entity.decoupled) {
-  //  const test = ipfs.cat(entity.dataHash)
-  //  if (test != null) {
-  //    log.info('IPFS success: {}', [test.toString()])
-  //    //entity.
-  //  }
-  //}
+  if (data.decoupled) {
+    let test = ipfs.cat(data.repoData)
+    if (test != null) {
+      let ipfsObj = json.fromBytes(test as Bytes).toObject()
+      data.name = ipfsObj.get('title').toString()
+    }
+  }
   // Entities can be written to the store with `.save()`
   repo.save()
   data.save()
@@ -132,7 +133,7 @@ export function handleIssueUpdated(event: IssueUpdated): void {
 
   let repoId = event.address.toHex() + '_' + event.params.repoId.toHex()
   let repoDecoupled = RepoData.load(repoId).decoupled
-  
+
   let issueId = repoId + '_' + event.params.issueNumber.toString()
   let issueData = IssueData.load(issueId)
   if (issueData == null) {
@@ -143,11 +144,11 @@ export function handleIssueUpdated(event: IssueUpdated): void {
     issue.issueNumber = event.params.issueNumber.toString()
     issue.save()
   }
+  issueData.repository = repoId
   issueData.number = event.params.issueNumber
-  issueData.repoHexId = event.params.repoId.toHexString()
+  issueData.repoHexId = repoId
   if (repoDecoupled) {
     issueData.repoId = event.params.repoId.toHexString()
-    issueData.repository = issueData.repoHexId
     issueData.labels = newLabels()
   } else {
     issueData.repoId = event.params.repoId.toString()
@@ -187,17 +188,18 @@ export function handleIssueUpdated(event: IssueUpdated): void {
       ) as Bytes
       issueData.standardBountyId = issueContractdata.value0
       issueData.deadline = ipfsObj.get('deadline').toString()
-      issueData.fundingHistories = []
+      issueData.fundingHistory = []
       getBountyIssue(issueData.standardBountyId.toString(), issueId)
       getFundingHistory(issueData as IssueData, ipfsObj.get('fundingHistory').toArray())
-      //log.info('funding histories: {}',[issueData.fundingHistories.length.toString()])
+      //log.info('funding histories: {}',[issueData.fundingHistory.length.toString()])
       //log.info('funding histories: {}',[getFundingHistory(issueId, ipfsObj.get('fundingHistory').toArray()).toString()])
       //issueData.fundingHistory = histories
-      log.info('about to save: {}',[issueData.fundingHistories.toString()])
+      log.info('about to save: {}',[issueData.fundingHistory.toString()])
     }
   }
   issueData.save()
 }
+
 
 function getBountyIssue(bountyId: string, issueId: string): void {
   let bounty = BountyIssue.load(bountyId)
@@ -209,7 +211,7 @@ function getBountyIssue(bountyId: string, issueId: string): void {
 }
 
 function getFundingHistory(issueData: IssueData, history: JSONValue[]): void {
-  let eventIds = issueData.fundingHistories
+  let eventIds = issueData.fundingHistory
   let issueId = issueData.id
   log.info('pre-Event ID: {}',[issueId])
   for (let index = 0; index < history.length; index++) {
@@ -227,7 +229,7 @@ function getFundingHistory(issueData: IssueData, history: JSONValue[]): void {
       eventIds.push(eventId)
     }
   }
-  issueData.fundingHistories = eventIds
+  issueData.fundingHistory = eventIds
   //log.info('event Ids saved',[])
   //issueData.save()
   //return eventIds as string
@@ -268,7 +270,7 @@ export function newUser(userObj: TypedMap<string, JSONValue>, addrBackup: string
   } else {
     addr = addrObj.toString()
   }
-  let id = login + '_' + addr 
+  let id = login + '_' + addr
   let user = User.load(id)
   if (user == null) {
     user = new User(id)
@@ -339,7 +341,7 @@ export function handleBountySettingsChanged(
 export function handleAssignmentRequested(event: AssignmentRequested): void {
   let projects = Contract.bind(event.address)
   let appNo = projects.getApplicantsLength(
-    event.params.repoId, 
+    event.params.repoId,
     event.params.issueNumber
   ).minus(BigInt.fromI32(1))
   log.info('application number: {}',[appNo.toString()])
